f9200a63d9b37b0ebd3ac2de141386b336ac894f
Christian Ehrnrooth <hce17356@gmail.com>
2017-08-15 11:40:47 -0400

    Update Problem10.py

--- Problem10.py
+++ Problem10.py
@@ -1,6 +1,9 @@
 #@author chehrnrooth, with help from the internet.
 sieve = [True]*2000000 #creates a sieve of 2 million
 def mark(sieve, x):
+	'''
+	takes a prime, finds its multiples and deletes them from the sieve. 
+	'''
 	for i in xrange(x+x, len(sieve), x): #skips around with step x
 		sieve[i] = False
 for x in xrange(2, int(len(sieve)**0.5)+1):
